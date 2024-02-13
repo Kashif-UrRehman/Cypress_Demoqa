64d8e99209f5bb023c42843faa02632727227eb4
Kashif ur Rehman <kashif.rehman@datumbrain.com>
2024-02-13 11:40:11 +0500

    .env added for app's secrets

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,12 @@
 const { defineConfig } = require("cypress");
 
+require("dotenv").config();
+
 module.exports = defineConfig({
+  // eslint-disable-next-line no-undef
+  env: { ...process.env },
   projectId: "yexqd8",
+
   e2e: {
     setupNodeEvents(_on, _config) {
       // implement node event listeners here

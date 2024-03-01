6f6b072fd72eae636375e6dfd170638b60cf03b8
Kashif ur Rehman <kashif.rehman@datumbrain.com>
2024-03-01 12:07:05 +0500

    Update following

--- cypress.config.js
+++ cypress.config.js
@@ -4,14 +4,16 @@ const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 require("dotenv").config();
 
 module.exports = defineConfig({
-  reporter: "cypress-mochawesome-reporter",
-  reporterOptions: {
-    charts: true,
-    reportPageTitle: "custom-title",
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
-  },
+  reporter: "mocha-allure-reporter",
+
+  // reporter: "cypress-mochawesome-reporter",
+  // reporterOptions: {
+  //   charts: true,
+  //   reportPageTitle: "custom-title",
+  //   embeddedScreenshots: true,
+  //   inlineAssets: true,
+  //   saveAllAttempts: false,
+  // },
   // eslint-disable-next-line no-undef
   env: { ...process.env },
   projectId: "yexqd8",
@@ -19,7 +21,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require("cypress-mochawesome-reporter/plugin")(on);
+      // require("cypress-mochawesome-reporter/plugin")(on);
       allureWriter(on, config);
       return config;
     },
